36b5cf9d48ddeabd0b8222c30f581c8b1f73b199
Brad Staton <brad.staton@gmail.com>
2024-02-15 18:41:01 -0500

    Fix missing import

--- src/python/scrape.py
+++ src/python/scrape.py
@@ -3,6 +3,7 @@
 import time
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
